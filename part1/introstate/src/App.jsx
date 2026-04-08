1463265e0cfb7733dcb1b48b51d3cf195838ae8a
aleemtariq <aleemtariq@protonmail.com>
2026-04-08 12:17:21 -0700

    Moved event handlers to separate functions.

--- part1/introstate/src/App.jsx
+++ part1/introstate/src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <button onClick={decreaseByOne}>minus</button>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
     </div>
   )
 }
