56282ae8718f9766a836aaa68bcc01c46c8888cc
aleemtariq <aleemtariq@protonmail.com>
2026-04-23 12:48:58 -0700

    Added more state.

--- part1/complexstate/src/App.jsx
+++ part1/complexstate/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-      <p>test</p>
+      {left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
