50deee385798be5789107c41da93ad8bf2421c99
aleemtariq <aleemtariq@protonmail.com>
2026-04-24 10:23:45 -0700

    Finished exercise 1.9.

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,5 +1,9 @@
 const Statistics = ({ good, bad, neutral }) => {
-    return (
+  const feedback = good != 0 || bad != 0 || neutral != 0;
+
+  return (
+    <div>
+      { feedback ? 
       <div>
         <h1>statistics</h1>
         <div>good {good}</div>
@@ -8,8 +12,15 @@ const Statistics = ({ good, bad, neutral }) => {
         <div>all {good + neutral + bad}</div>
         <div>average {(good - bad)/(good + neutral + bad)}</div>
         <div>positive {(good)/(good + neutral + bad) * 100}%</div>
+      </div> 
+      : 
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
       </div>
-    )
-  }
+      }
+    </div>
+  )
+}
 
   export default Statistics
