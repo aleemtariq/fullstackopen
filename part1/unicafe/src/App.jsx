cbfe567e7a2cdff52fd482023339ba339c955aa4
aleemtariq <aleemtariq@protonmail.com>
2026-04-24 10:52:57 -0700

    Finished exercise 1.10.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Statistics from './components/Statistics'
+import Button from './components/Button'
 
 const App = () => {
   // save clicks of each button to its own state
@@ -7,12 +8,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button text={'good'} onClick={handleClickGood} />
+        <Button text={'neutral'} onClick={handleClickNeutral} />
+        <Button text={'bad'} onClick={handleClickBad} />
         <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,3 +1,14 @@
+const StatisticLine = ({ text, value, percent }) => {
+  return (
+    <div>
+      { percent ? 
+        <>{text} {value * 100}% </> : 
+        <>{text} {value}</>
+      }
+    </div>
+  )
+}
+
 const Statistics = ({ good, bad, neutral }) => {
   const feedback = good != 0 || bad != 0 || neutral != 0;
 
@@ -6,12 +17,12 @@ const Statistics = ({ good, bad, neutral }) => {
       { feedback ? 
       <div>
         <h1>statistics</h1>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {good + neutral + bad}</div>
-        <div>average {(good - bad)/(good + neutral + bad)}</div>
-        <div>positive {(good)/(good + neutral + bad) * 100}%</div>
+        <StatisticLine text={"good"} value={good} />
+        <StatisticLine text={"neutral"} value={neutral} />
+        <StatisticLine text={"bad"} value={bad} />
+        <StatisticLine text={"all"} value={good + neutral + bad} />
+        <StatisticLine text={"average"} value={(good - bad)/(good + neutral + bad)} />
+        <StatisticLine text={"positive"} value={(good)/(good + neutral + bad)} percent={true} />
       </div> 
       : 
       <div>
